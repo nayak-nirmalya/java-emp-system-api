05eb23b22b6cdcbdb306d880c8a91fdfafb9b06a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-27 22:08:22 +0530

    added route for get all emp details

--- src/main/java/com/nirmalya/Employee/services/EmployeeService.java
+++ src/main/java/com/nirmalya/Employee/services/EmployeeService.java
@@ -2,6 +2,10 @@ package com.nirmalya.Employee.services;
 
 import com.nirmalya.Employee.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
 }
