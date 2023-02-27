7fd6943b3399fb4da4327146b27bc40a2d41e050
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-27 19:51:23 +0530

    changed cross orignin and jdbc driver class

--- src/main/java/com/nirmalya/Employee/controller/EmployeeController.java
+++ src/main/java/com/nirmalya/Employee/controller/EmployeeController.java
@@ -3,11 +3,9 @@ package com.nirmalya.Employee.controller;
 import com.nirmalya.Employee.model.Employee;
 import com.nirmalya.Employee.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
