f4c8566535dff4459635adde6945223d8d7f7d12
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-27 15:07:15 +0530

    created model, controller, entity and repo

---
+++ src/main/java/com/nirmalya/Employee/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.nirmalya.Employee.controller;
+
+import com.nirmalya.Employee.model.Employee;
+import com.nirmalya.Employee.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+}
---
+++ src/main/java/com/nirmalya/Employee/services/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.nirmalya.Employee.services;
+
+import com.nirmalya.Employee.model.Employee;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+}
