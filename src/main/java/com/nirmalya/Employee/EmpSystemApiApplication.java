8340ad10120b0bae110e23c4fc614a3a38883dba
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-27 09:35:15 +0530

    first commit

---
+++ src/main/java/com/nirmalya/Employee/EmpSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.nirmalya.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nirmalya/Employee/EmpSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirmalya.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
